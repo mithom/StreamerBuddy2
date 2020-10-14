637cd225a887476bb78dcccc4b0fb196fdc4fbbe
thomas michiels <thomas.michiels@external.atlascopco.com>
2020-10-14 16:31:42 +0200

    apply window typedeclaration only to vue part

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface Window {
-    ipcRenderer: {
-        invoke(channel: string, ...args: any[]): Promise<unknown>;
-        once(channel: string, func: (...args: any[]) => void): Promise<void>;
-    };
-}
-
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,12 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent
+  const component: typeof DefineComponent
   export default component
+}
+
+declare interface Window {
+  ipcRenderer: {
+    invoke(channel: string, ...args: any[]): Promise<unknown>;
+    once(channel: string, func: (...args: any[]) => void): Promise<void>;
+  };
 }
