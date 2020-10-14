307e96513c266f4f874786e5ab1600affda28e00
thomas michiels <thomas.michiels@external.atlascopco.com>
2020-10-14 18:13:29 +0200

    add tailwindcss and layout updater

--- src/components/Updater.vue
+++ src/components/Updater.vue
@@ -1,17 +1,27 @@
 <template>
-<div v-if="updateAvailable">
-  <p>new version available: {{version?.version}}</p>
-  <template v-if="!(downloading || downloaded)">
-    <button @click="download(false)">download</button>
-    <button @click="download(true)">download and install</button>
-  </template>
-  <p v-if="downloading">progress: {{progress}}%</p>
-  <template v-if="downloaded">
-    <p>completed download</p>
-    <button @click="install">install</button>
-  </template>
-
-</div>
+  <transition name="slide-fade">
+    <div v-if="updateAvailable" class="w-full absolute top-0 flex justify-center items-center bg-green-400 py-2 rounded-b-lg font-bold text-white">
+      <p class="mr-5">new version available: {{version?.version}}</p>
+      <template v-if="!(downloading || downloaded)">
+        <button
+            class="btn-blue"
+            @click="download(false)"
+        >download</button>
+        <button
+            class="btn-blue"
+            @click="download(true)"
+        >download and install</button>
+      </template>
+      <p v-if="downloading">progress: {{progress}}%</p>
+      <template v-if="downloaded">
+        <p class="mr-5">download complete</p>
+        <button
+            class="btn-blue"
+            @click="close & install"
+        >install</button>
+      </template>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -57,5 +67,22 @@ export default defineComponent({
 </script>
 
 <style scoped>
+@layer components {
+  .btn-blue{
+    @apply bg-blue-500 text-white font-bold py-2 px-4 rounded mx-2;
+  }
+  .btn-blue:hover{
+    @apply bg-blue-700;
+  }
+}
 
+.slide-fade-enter-active {
+  transition: transform 0.5s ease-out, opacity 0.5s ease-in;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translatey(-100%);
+  opacity: 0;
+}
 </style>
