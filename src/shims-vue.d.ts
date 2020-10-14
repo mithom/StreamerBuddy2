dae2388d299e11128e25fa908e3a30f4c3375fc0
thomas michiels <thomas.michiels@external.atlascopco.com>
2020-10-14 16:36:24 +0200

    put global types in their own file

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,12 +1,8 @@
+import type types from '@/types/vueGlobal';
+export default types;
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: typeof DefineComponent
   export default component
-}
-
-declare interface Window {
-  ipcRenderer: {
-    invoke(channel: string, ...args: any[]): Promise<unknown>;
-    once(channel: string, func: (...args: any[]) => void): Promise<void>;
-  };
 }
---
+++ src/types/vueGlobal.d.ts
@@ -0,0 +1,6 @@
+declare interface Window {
+    ipcRenderer: {
+        invoke(channel: string, ...args: any[]): Promise<unknown>;
+        once(channel: string, func: (...args: any[]) => void): Promise<void>;
+    };
+}
